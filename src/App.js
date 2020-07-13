feeb909a9afde320efb22d99b50b87a1a1900d04
shdev2018 <shaunhall.dev@gmail.com>
2020-07-13 21:25:07 +0100

    Dynamic styling.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,26 @@ function App() {
   const date = new Date()
   const hours = date.getHours()
   let timeOfDay
+  const styles = { fontSize: 20 }
 
   if (hours < 12) {
     timeOfDay = 'morning'
+    styles.color = '#04756F'
   }
   else if (hours >= 12 && hours < 17) {
     timeOfDay = 'afternoon'
+    styles.color = '#8914A3'
   }
   else {
     timeOfDay = 'night'
+    styles.color = '#D90000'
   }
   
+  
+
   return (
     <div>
-      <h1>Good {timeOfDay}!</h1>
+      <h1 style={styles}>Good {timeOfDay}!</h1>
     </div>
   )
 }
