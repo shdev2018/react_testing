eb15e2d08b7907ad37ac8fd8273d75c013d4e2db
shdev2018 <shaunhall.dev@gmail.com>
2020-07-13 21:06:12 +0100

    More complex JS and interpolation.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ToDoList from './Components/ToDoList';
-import NavBar from './Components/NavBar';
 import './App.css';
 
 function App() {
-  const fName = "Shaun"
-  const lName = "Hall"
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay
 
+  if (hours < 12) {
+    timeOfDay = 'morning'
+  }
+  else if (hours >= 12 && hours < 17) {
+    timeOfDay = 'afternoon'
+  }
+  else {
+    timeOfDay = 'night'
+  }
+  
   return (
     <div>
-      <h1>Hello { `${fName} ${lName}` }</h1>
-      <NavBar />
-      <ToDoList />
+      <h1>Good {timeOfDay}!</h1>
     </div>
   )
 }
